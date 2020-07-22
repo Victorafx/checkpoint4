0c8e4566d8327cb584e2c58561e7875e8ca24e3b
Victorafx <victor.auxietrefontes@laposte.net>
2020-07-22 15:12:57 +0200

    create navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className="navbar_block">
+      <p>Profil</p>
+      <p>Compétences</p>
+      <p>Portfolio</p>
+      <p>C.V</p>
+      <p>Contact</p>
+    </div>
+  )
+}
+
+export default Navbar;
