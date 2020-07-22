ee33951435b9a5e3024d88f3a03bff7e145f8944
Victorafx <victor.auxietrefontes@laposte.net>
2020-07-22 15:30:22 +0200

    import presentation with some css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Navbar from './Components/Navbar';
+import Presentation from './Components/Presentation';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Presentation />
       </div>
     );
   }
