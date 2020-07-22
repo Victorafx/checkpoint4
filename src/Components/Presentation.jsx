cdf7dd9fbe4c477cf61e4357ef9e4bad903d6b7e
Victorafx <victor.auxietrefontes@laposte.net>
2020-07-22 15:29:57 +0200

    create component presentation

---
+++ src/Components/Presentation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+const Presentation = () => {
+  return (
+    <div className="presentation_block">
+      <h1>Victor Auxietre-Fontes</h1>
+      <h2>Développeur Web Junior</h2>
+    </div>
+  )
+}
+
+export default Presentation;
